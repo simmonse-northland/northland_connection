dc3543b28499668d3c4809257d00009da750a5ce
Eric Simmons <simmonse@northlandbuildings.com>
2023-05-01 13:32:21 -0500

    join working

--- db.py
+++ db.py
@@ -15,20 +15,32 @@ def connect():
         print("Error", e)
         return None
 
-def get_trim_by_estimate_id():
+def get_trim_by_estimate_id(estimate_id):
+    # sql = """
+    # USE NBEstTransmitted;
+    # SELECT Description, COLOR, ORDQTY
+    # FROM dbo.Customers_Detail_tbl
+    # WHERE EstimateID = 150985 AND Category = 'Trim'
+    # """
+    # sql="""
+    # USE NBEstTransmitted;
+    # SELECT FIRSTNAME, LASTNAME, Order_Date
+    # FROM dbo.Customers_Main_tbl
+    # WHERE EstimateID = 150985 """
     sql = """
-    USE NBEstTransmitted;
-    SELECT Description, COLOR, ORDQTY
-    FROM dbo.Customers_Detail_tbl
-    WHERE EstimateID = 150985 AND Category = 'Trim'
-    """
+        USE NBEstTransmitted;
+        SELECT cdm.Description, cdm.COLOR, cdm.ORDQTY, cmt.FIRSTNAME, cmt.LASTNAME, cmt.Order_Date
+        FROM dbo.Customers_Detail_tbl cdm
+        JOIN dbo.Customers_Main_tbl cmt ON cdm.EstimateID = cmt.EstimateID
+        WHERE cdm.EstimateID = 150985 AND cdm.Category = 'Trim'
+        """
     conn = connect()
     if conn:
         cursor = conn.cursor()
         cursor.execute(sql)
         result = cursor.fetchall()
         conn.close()
-        table = PrettyTable(['Description', 'Color', 'Order Quantity'])
+        table = PrettyTable(['Description', 'Color', 'Order Quantity', 'col4', 'col5', 'col6'])
 
         # Add the data rows to the table
         for row in result:
@@ -41,4 +53,4 @@ def get_trim_by_estimate_id():
         return None
     
 if __name__ == "__main__":
-    get_trim_by_estimate_id()
+    get_trim_by_estimate_id(150985)
