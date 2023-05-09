c1d0c1823b499ce902e2fdb7eae804850723773b
Eric Simmons <simmonse@northlandbuildings.com>
2023-05-09 13:50:42 -0500

    frontend updated

--- GenerateReport.py
+++ GenerateReport.py
@@ -149,28 +149,7 @@ class GenerateReport():
             data = [(description, color, qty)]
             filename = f"{description} {color} {qty}".replace('/', '_').replace('"', '').replace(':', '') + ".pdf"
             cls.generate_pdf(filename, cls.generate_html_report(headers, data), headers, options)
-
-    @classmethod
-    def split_pdf(cls, directory):
-        files = os.listdir(directory)
-        for file in files:
-            if file.endswith('.pdf'):
-                pdf_path = os.path.join(directory, file)
-                with open(pdf_path, 'rb') as pdf_file:
-                    pdf_reader = PdfReader(pdf_file)
-                    num_pages = len(pdf_reader.pages)
-                    
-                    if num_pages > 1:
-                        for page_num in range(num_pages):
-                            new_filename = os.path.splitext(file)[0] + f'_page{page_num+1}.pdf'
-                            pdf_writer = PdfWriter()
-                            pdf_writer.add_page(pdf_reader.pages[page_num])
-                            pdf_writer.write(os.path.join(directory, new_filename))
-                        pdf_file.close()
-                        os.remove(pdf_path)
-                        print(f"File {pdf_path} has been split into {num_pages} pages.")
-                    else:
-                        print(f"File {pdf_path} has only 1 page, no need to split.")
+               
     @classmethod
     def open_foxit(cls, folder):
         foxit_reader_path = r"C:\Program Files (x86)\Foxit Software\Foxit PDF Reader\FoxitPDFReader.exe"
--- app.py
+++ app.py
@@ -20,30 +20,31 @@ class CVS(tk.Tk):
         scrollbar.config(command=self.contract_listbox.yview)
         contract_label = tk.Label(self, text="Select a Contract Number:")
         contract_label.pack()
+
         # populate listbox
         for contract_number in self.contract_numbers:
-            self.contract_listbox.insert(tk.END, contract_number)
+            self.contract_listbox.insert(tk.END, str(contract_number[0]))
+        
         self.contract_listbox.bind('<<ListboxSelect>>', self.select_contract)
-
-
         self.contract_data_frame = tk.Frame(self)
         self.contract_data_frame.pack(side='left', fill='both', expand=True)
         
         generate_report_button = tk.Button(self, text="Generate Report", command=self.generate_report)
         generate_report_button.pack()
 
-        # view_contract_button = tk.Button(self, text="View Contract", command=self.view_contract)
-
-
     def generate_report(self):
         # Get the selected contract number and generate the report
-        contract_number = int(self.contract_var.get())
+        selected_contract = self.contract_listbox.get(tk.ACTIVE)
+        contract_number = int(selected_contract)
+        print(selected_contract, contract_number)
         data = OrderData.get_trim(contract_number)
         headers = OrderData.get_headers_for_trim_labels(contract_number)
         print(headers)
         if data:
             GenerateReport.generate_report_all_trim(headers, data)
             GenerateReport.generate_report_each_trim(headers, contract_number)
+            GenerateReport.generate_report_title(headers)
+            GenerateReport.open_foxit(fr'H:\basic_csv_transfer\{contract_number}_trim_prints')
             print("Reports Generated!")
         else:
             print("Couldn't generate report")
@@ -56,14 +57,13 @@ class CVS(tk.Tk):
         if selected_index:
             # get the value of the selected item
             selected_contract = self.contract_listbox.get(selected_index[0])
-            contract_data = OrderData.get_contract_data(selected_contract)
+            contract_data = OrderData.get_one_contract(selected_contract)
+            print(type(contract_data))
             if contract_data:
                 # add the contract data to the contract_data_frame
-                for i, (key, value) in enumerate(contract_data.items()):
-                    label_key = tk.Label(self.contract_data_frame, text=key)
-                    label_key.grid(row=i, column=0, sticky='w')
+                for i, value in enumerate(contract_data):
                     label_value = tk.Label(self.contract_data_frame, text=value)
-                    label_value.grid(row=i, column=1, sticky='w')
+                    label_value.grid(row=i, column=0, sticky='w')
 
                     
 if __name__ == '__main__':
